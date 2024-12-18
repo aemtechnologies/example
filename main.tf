b2fccd28b35e6a6c77788ef3707f7482c69b6e59
aemtechnologies <65249478+aemtechnologies@users.noreply.github.com>
2024-12-18 21:42:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA3KJLEZFJ3CPR7JFO"
-  secret_key = "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"
+  access_key = "AKIA3KJLEZFJ4VOOKSHY"
+  secret_key = "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"
 }
 
 resource "aws_instance" "webserver" {
